7370d6f6610edffce022f2f4d75b9c92311a601a
msantanal <108295791+msantanal@users.noreply.github.com>
2022-06-30 18:21:20 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "rg" {
-  name     = var.rg_name
-  location = var.rg_location
+resource "azurerm_resource_group" "RG" {
+  name     = var.resource_group_name
+  location = var.location
 }
